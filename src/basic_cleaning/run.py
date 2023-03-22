bf2a127e7e355cf7604cb44b1893ac8f90f1e0bf
Theresa Fruhwuerth (TV-D-11) <Theresa.Fruhwuerth@bmw.de>
2023-03-22 10:43:25 +0100

    add basic data cleaning step

---
+++ src/basic_cleaning/run.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+"""
+Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
+"""
+import argparse
+import os
+import logging
+import wandb
+import pandas as pd
+import numpy as np
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def go(args):
+    run = wandb.init(job_type="basic_cleaning")
+    run.config.update(args)
+
+    logging.info('Downloading raw dataset from Wandb.')
+    if not os.path.exists('../../components/get_data/data/' + args.input_artifact):
+        logging.error('ERROR: Please include the download step in the pipeline.')
+
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+    df = pd.read_csv(artifact_local_path)
+    logging.info('Downloading raw dataset SUCCESS.')
+
+    logging.info('Preprocessing raw dataset.')
+    df = df.drop_duplicates('name')
+    assert len(df.name.unique()) == len(df)
+    df.index = df.name
+    df['name'].fillna(df['host_id'], inplace=True)
+    df['lognorm_minimum_nights'] = np.log(df.minimum_nights)
+    idx = df['price'].between(args.min_price, args.max_price)
+    df = df[idx].copy()
+    df['last_review'] = pd.to_datetime(df['last_review'])
+    df.availability_365.fillna(df.availability_365.median(), inplace=True)
+    keepcols = set(df.columns).difference(set(['host_id', 'reviews_per_month',
+                                               'latitude', 'longitude',
+                                               'minimum_nights']))
+    df_new = df[keepcols]
+    logging.info('Preprocessing SUCCESS.')
+    logging.info(f'Keeping columns {keepcols}')
+
+    logging.info('Uploading preprocessed dataset.')
+
+    df_new.to_csv(args.output_artifact, index=False)
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    artifact.add_file(args.output_artifact)
+    run.log_artifact(artifact)
+
+    logging.info('Uploading SUCCESS.')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="A very basic data cleaning")
+    parser.add_argument(
+        "--input_artifact",
+        type=str,
+        help='Path for input artifact download.',
+        required=True,
+        default="sample.csv"
+    )
+
+    parser.add_argument(
+        "--output_artifact",
+        type=str,
+        help='Path to output artifact location.',
+        required=True,
+        default="clean_sample.csv"
+    )
+
+    parser.add_argument(
+        "--output_type",
+        type=str,
+        help='Type of file that is output. e.g. csv',
+        required=True,
+        default='csv'
+    )
+
+    parser.add_argument(
+        "--output_description",
+        type=str,
+        help='Type of file that is input e.g. csv',
+        required=True,
+        default='Preprocessed Input Datafile.'
+    )
+
+    parser.add_argument(
+        "--min_price",
+        type=float,
+        help='Minimum price to be considered not an outlier.',
+        required=True,
+        default=10
+    )
+
+    parser.add_argument(
+        "--max_price",
+        type=float,
+        help='Maximum price to be considered not an outlier.',
+        required=True,
+        default=350,
+    )
+
+    args = parser.parse_args()
+
+    go(args)
